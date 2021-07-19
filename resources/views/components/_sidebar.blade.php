420728b044873fc363a798811a6ea8ae71efecfa
Aidillia <aidilliyaadhahsb@gmail.com>
2021-07-19 22:42:38 +0700

    kumpul tugas

---
+++ resources/views/components/_sidebar.blade.php
@@ -0,0 +1,23 @@
+<div class="sidebar">
+    <div class="app-title">Quis Web</div>
+
+    <div class="sidebar-header">
+        <img src="{{ asset('images/profile-img.png') }}" alt="">
+        <div class="name-tag">
+            <div class="name">Aidillia Adha Hsb</div>
+            <div class="status">Admin</div>
+        </div>
+    </div>
+
+    <div class="sidebar-menu-tag">Menu</div>
+    <div class="sidebar-content">
+        <ul class="sidebar-menu">
+            <li>
+                <a href="#" class="sidebar-menu-item">
+                    <i class="fas fa-desktop sidebar-icon-img"></i>
+                    <div class="sidebar-text-item">Dashboard</div>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
